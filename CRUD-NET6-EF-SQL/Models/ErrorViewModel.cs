2d4ca741d7b09a4c7fe58cdca84dd0be730da4eb
D3atHpR <kastiel103@gmail.com>
2022-04-08 00:38:19 -0300

    Adicionar arquivos de projeto.

---
+++ CRUD-NET6-EF-SQL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CRUD_NET6_EF_SQL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
